061549672c67afe9904ecfa655ec8ce9b219ac7c
Nguyễn Văn Thành Đại <Dai2272005nv@gmail.com>
2024-11-30 12:07:32 +0700

    Update Webbandoan

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,6 @@
-document.querySelectorAll('.inner-img a').forEach(function (item) {
-  item.addEventListener('click', function (event) {
+document.querySelectorAll(".inner-img a").forEach(function (item) {
+  item.addEventListener("click", function (event) {
     event.preventDefault();
-
   });
 });
 
@@ -14,15 +13,15 @@ function thanhToan() {
 }
 
 let lastScrollY = window.scrollY;
-const headerBottom = document.querySelector('.header-bottom');
+const headerBottom = document.querySelector(".header-bottom");
 
-window.addEventListener('scroll', () => {
+window.addEventListener("scroll", () => {
   if (window.scrollY > lastScrollY) {
-    headerBottom.classList.add('hide');
-    headerBottom.classList.remove('show');
+    headerBottom.classList.add("hide");
+    headerBottom.classList.remove("show");
   } else {
-    headerBottom.classList.add('show');
-    headerBottom.classList.remove('hide');
+    headerBottom.classList.add("show");
+    headerBottom.classList.remove("hide");
   }
   lastScrollY = window.scrollY;
 });
@@ -45,19 +44,35 @@ function notLogin() {
 
 document.addEventListener("DOMContentLoaded", function () {
   // Lấy tất cả các nút accordion
-  const accordionButtons = document.querySelectorAll('.PhuongThuc .btn');
+  const accordionButtons = document.querySelectorAll(".PhuongThuc .btn");
 
-  accordionButtons.forEach(button => {
-    button.addEventListener('click', function () {
+  accordionButtons.forEach((button) => {
+    button.addEventListener("click", function () {
       // Xóa lớp grayscale khỏi tất cả các nút
-      accordionButtons.forEach(btn => btn.classList.remove('grayscale'));
+      accordionButtons.forEach((btn) => btn.classList.remove("grayscale"));
 
       // Áp dụng lớp grayscale cho tất cả các nút trừ nút đang được nhấn
-      accordionButtons.forEach(btn => {
+      accordionButtons.forEach((btn) => {
         if (btn !== this) {
-          btn.classList.add('grayscale');
+          btn.classList.add("grayscale");
         }
       });
     });
   });
+});
+
+let result = document.querySelector("#tanggiam").value;
+let tang = document.querySelector("#tang");
+let giam = document.querySelector("#giam");
+
+tang.addEventListener("click", () => {
+  result++;
+  document.querySelector("#tanggiam").value = result;
+});
+
+giam.addEventListener("click", () => {
+  if (result > 1) {
+    result--;
+    document.querySelector("#tanggiam").value = result;
+  }
 });
